b272e98618da0aea38391fcb2c3e8a5726dc57b5
John Doe <nickborcic69@gmail.com>
2024-03-07 14:14:14 +0100

    ratings average rating set and displayng corretlly

---
+++ my-app/src/lib/stars.ts
@@ -0,0 +1,14 @@
+import {spots } from "$lib/sampleData";
+export const stars=[
+
+    {name:"color", star:"",color:"orange"} ,
+ {name:"color1", star1:"★"} ,
+ {name:"color2", star2:"★★"} ,
+ {name:"color3", star3:"★★★"} ,
+ {name:"color4", star4:"★★★★"} ,
+ {name:"color5", star5:"★★★★★"} ,
+
+  
+];
+
+export let starsGrap = ["★", "★", "★", "★", "★"];
