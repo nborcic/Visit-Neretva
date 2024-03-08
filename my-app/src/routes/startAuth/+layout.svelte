631401d94602f3210dd4c213b4085699c9715cc3
John Doe <nickborcic69@gmail.com>
2024-03-08 11:25:28 +0100

    login page @startAuth done, makeing changed to a storing stuff somewhere :*

---
+++ my-app/src/routes/startAuth/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+ 
+</script>
+
+<div class="">
+<div class="">
+  <div class="">
+    <div class="">
+      <main class="">
+        <slot />
+      </main>
+    </div>
+  </div>
+</div>
+</div>
