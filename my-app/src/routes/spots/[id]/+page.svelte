d45bbeb52ef16398141a120a87b73aaf2ecaa10c
John Doe <nickborcic69@gmail.com>
2024-03-11 21:27:57 +0100

    alert mesage and pop up done, mising js on stars

--- my-app/src/routes/spots/[id]/+page.svelte
+++ my-app/src/routes/spots/[id]/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { onMount } from "svelte";
+  import Swal from "sweetalert2";
   export let data;
   export function starOne() {
     console.log(starOne);
@@ -18,12 +20,19 @@
   export function submitStar() {
     console.log("submitStar");
   }
-  function alertClick() {
-    const click = document.getElementById("#alertClick");
-    ////////////////make and alert for comfirmation
+
+  function subPressed() {
+    
+   Swal.fire({
+      position: "top-end",
+      icon: "success",
+      title: "Thanks!",
+      showConfirmButton: false,
+      timer: 1500,
+    });
   }
 
-  const testAlert = () => alert("alert."); 
+
 </script>
 
 <div
@@ -132,6 +141,10 @@
       <div class="inline-flex items-center flex-col gap-1">
         <h3 class="flex">Rate this place!</h3>
         <div class="bodyOne flex">
+          <!-- svelte-ignore a11y-click-events-have-key-events -->
+          <!-- svelte-ignore a11y-click-events-have-key-events -->
+          <!-- svelte-ignore a11y-no-static-element-interactions -->
+          <!-- svelte-ignore a11y-no-static-element-interactions -->
           <div class="rating text-amber-500">
             <span id="star5" on:click={starFive}>☆</span><span
               on:click={starFour}
@@ -144,7 +157,7 @@
           <div class="flex flex-col">
             <button
               id="submitStar"
-              on:click={testAlert}
+              on:click={subPressed}
               type="submit"
               class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-6 border border-blue-500 hover:border-transparent rounded"
               >Rate</button
@@ -177,38 +190,6 @@
       </div>
     </div>
   </div>
-</div>
-
-<div class="px-8 py-6 bg-green-400 text-white flex justify-between rounded">
-  <div class="flex items-center">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      class="h-7 w-7 mr-6"
-      viewBox="0 0 20 20"
-      fill="currentColor"
-    >
-      <path
-        d="M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z"
-      />
-    </svg>
-    <p>Success! This is a success alert—check it out!</p>
-  </div>
-  <button class="text-green-100 hover:text-white">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      class="h-6 w-6"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-    >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        d="M6 18L18 6M6 6l12 12"
-      />
-    </svg>
-  </button>
 </div>
 
 <style>
